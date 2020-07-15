24a36cfd5052968e0898c991e1056e2a7a8120c4
godqhrgo7849 <63216333+godqhrgo7849@users.noreply.github.com>
2020-07-15 09:56:36 +0900

    07/15(30303김재홍)
    1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -59,6 +59,7 @@ public class PlayerController : MonoBehaviour {
         playerAudio.Play();
         playerRigidbody.velocity = Vector2.zero;
         isDead = true;
+        GameManager.instance.OnPlayerDead();
         // 사망 처리
     }
 
